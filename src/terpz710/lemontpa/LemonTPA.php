00787a08efa2e9dced6d41a6b63f63b96107d437
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-04-10 15:35:32 -0700

    extra prot

--- src/terpz710/lemontpa/LemonTPA.php
+++ src/terpz710/lemontpa/LemonTPA.php
@@ -11,6 +11,7 @@ use terpz710\lemontpa\commands\TPAHereCommand;
 use terpz710\lemontpa\commands\TPAcceptCommand;
 
 use CortexPE\Commando\PacketHooker;
+use CortexPE\Commando\BaseCommand;
 
 use DaPigGuy\libPiggyUpdateChecker\libPiggyUpdateChecker;
 
@@ -23,6 +24,8 @@ class LemonTPA extends PluginBase {
     }
 
     protected function onEnable() : void{
+        $this->checkVirions();
+        
         libPiggyUpdateChecker::init($this);
 
         if (!PacketHooker::isRegistered()) {
@@ -38,5 +41,20 @@ class LemonTPA extends PluginBase {
 
     public static function getInstance() : self{
         return self::$instance;
+    }
+
+    private function checkVirions() : void{
+        foreach (
+            [
+                "Commando" => BaseCommand::class,
+                "libPiggyUpdateChecker" => libPiggyUpdateChecker::class
+            ] as $virion => $class
+        ) {
+            if (!class_exists($class)) {
+                $this->getLogger()->error($virion . " virion not found. Download LemonTPA at https://poggit.pmmp.io/ci/Terpz710/LemonTPA/LemonTPA");
+                $this->getServer()->getPluginManager()->disablePlugin($this);
+                return;
+            }
+        }
     }
 }
